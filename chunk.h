018e33241b8861e962bb745b3463a68961869ab2
Eirik Nielsen <eirik.niel@gmail.com>
2018-02-20 18:21:42 +0100

    finished chapter 1

---
+++ chunk.h
@@ -0,0 +1,29 @@
+#ifndef clox_clunk_h
+#define clox_clunk_h
+
+#include "common.h"
+#include "value.h"
+
+typedef enum {
+    OP_CONSTANT,
+    OP_RETURN,
+} OpCode;
+
+typedef struct {
+    int count;
+    int capacity;
+    uint8_t* code;
+    int* lines;
+    ValueArray constants;
+} Chunk;
+
+void initChunk(Chunk* chunk);
+
+void freeChunk(Chunk* chunk);
+
+void writeChunk(Chunk* chunk, uint8_t byte, int line);
+
+int addConstant(Chunk* chunk, Value value);
+
+
+#endif
